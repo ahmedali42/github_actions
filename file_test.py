ef2472d5486bada3557a330508f7a7fbd123841f
ahmedali42 <ahmedali.4290@gmail.com>
2025-06-25 12:08:43 +0000

    update

--- file_test.py
+++ file_test.py
@@ -1,17 +1,22 @@
-import pytest
-
-def test_calc_addition(): 
-    output = 2 + 4 
+def test_calc_addition():
+    # Function to test the output of 2 + 4
+    output = 2 + 4
     assert output == 6
 
-def test_calc_subtraction():  
+
+def test_calc_subtraction():
+    # Function to test the output of 2 - 4
     output = 2 - 4
     assert output == -2
 
-def test_calc_multiply(): 
-    output = 2 * 4 
-    assert output == 8 
 
-def test_coucou(): 
-    output = 'hello' 
+def test_calc_multiply():
+    # Function to test the output of 2 * 4
+    output = 2 * 4
+    assert output == 8
+
+
+def test_coucou():
+    # Function to test if the output returns 'hello'
+    output = 'hello'
     assert output == 'hello'
